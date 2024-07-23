c791a604d01e910380b4cf3a9633b8386920d4c7
froogers28 <danield@qpmc.net>
2024-07-23 09:46:29 -0400

    Updated for ESP8266, still not working

--- nfc/sketch_jul22a/sketch_jul22a.ino
+++ nfc/sketch_jul22a/sketch_jul22a.ino
@@ -1,15 +1,25 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include "config.h"
 
-#define RST_PIN 9
-#define SS_PIN 10
+#define RST_PIN 4
+#define SS_PIN 5
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-
+AdafruitIO_Feed *nfc = io.feed("nfc");
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial)
     ;
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+  while (io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println();
+  Serial.println(io.statusText());
   SPI.begin();
   mfrc522.PCD_Init();
   delay(4);
@@ -27,14 +37,23 @@ void loop() {
     return;
   }
   String userid;
+  String last;
   for (byte i = 0; i < mfrc522.uid.size; i++) {
     userid += String(mfrc522.uid.uidByte[i], HEX);
   }
+  if (userid == last)
+    Serial.println("same");
+  return;
   Serial.println(userid);
-  if (userid == "4e54722467680"){
+  if (userid == "4e54722467680") {
+    Serial.print("sending value -> ");
     Serial.println("yes");
+    nfc->save("yes");
   }
-  if (userid != "4e54722467680"){
+  if (userid != "4e54722467680") {
+    Serial.print("sending value -> ");
     Serial.println("no");
+    nfc->save("no");
   }
+  last = userid;
 }
